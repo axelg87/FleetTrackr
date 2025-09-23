ed592ff2f2330457dea7d6ce6157b987374aa71a
axelg87 <83286391+axelg87@users.noreply.github.com>
2025-09-23 10:38:29 +0400

    Fix keyboard options imports for dialogs

--- app/src/main/java/com/fleetmanager/ui/components/CarDialogs.kt
+++ app/src/main/java/com/fleetmanager/ui/components/CarDialogs.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,7 +23,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.fleetmanager.domain.model.Car
