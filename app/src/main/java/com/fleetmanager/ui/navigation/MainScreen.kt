dd1c36f518cd3b0636982123481d6a89be3936ed
axelg87 <83286391+axelg87@users.noreply.github.com>
2025-09-28 00:28:20 +0400

    Refine dashboard dark theme styling

--- app/src/main/java/com/fleetmanager/ui/navigation/MainScreen.kt
+++ app/src/main/java/com/fleetmanager/ui/navigation/MainScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -76,6 +77,8 @@ fun MainScreen(
     }
 
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground,
         bottomBar = {
             BottomNavigationBar(
                 items = bottomNavItems,
