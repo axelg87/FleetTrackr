ec934f16ce0cde70095a0d9b8294300ee47b94dc
axelg87 <83286391+axelg87@users.noreply.github.com>
2025-10-01 12:10:44 +0400

    Set splash screen background to pure black (#125)

--- app/src/main/java/com/fleetmanager/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/fleetmanager/ui/screens/splash/SplashScreen.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.fleetmanager.R
 import kotlinx.coroutines.delay
@@ -27,7 +27,7 @@ fun SplashScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(Color.Black),
         contentAlignment = Alignment.Center
     ) {
         Image(
